c7cf55b70fd3ea7fa504c27a5e6d793cc2d7051a
lapinzon <elpinzon@gmail.com>
2024-01-09 00:16:50 -0400

    [ADD] res_currency_rate_provider_BCV: Obtain rates from BCV

---
+++ currency_rate_update_bcv/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "currency_rate_update_bcv",
+    "summary": """OCA version for BCV scrapping rates
+       """,
+    "version": "16.0.1.1.2",
+    "development_status": "Beta",
+    "category": "Financial Management/Configuration",
+    "website": "https://github.com/OCA/l10n-venezuela",
+    "author": "Luis Pinzón, Odoo Community Association (OCA)",
+    "maintainers": ["lapinzon"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["currency_rate_update"],
+    "data": [
+        "views/views.xml",
+    ],
+}
