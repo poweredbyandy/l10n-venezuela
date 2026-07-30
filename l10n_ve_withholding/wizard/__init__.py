f3e774a2a502150a593cdf33be8253e9fd80fe5c
andyengit <anderson.armeya@gmail.com>
2026-07-30 06:14:10 +0000

    [ADD] l10n_ve_withholding: implement account move validation and retention creation
    - Added a new wizard for validating account moves, which triggers the creation of post retentions.
    - Updated the action_post method in the account move model to ensure retentions are created after posting.
    - Enhanced the import structure to include the new validation wizard for better modularity.

--- l10n_ve_withholding/wizard/__init__.py
+++ l10n_ve_withholding/wizard/__init__.py
@@ -1,4 +1,5 @@
 from . import account_payment_register
+from . import account_validate_account_move
 from . import arcv_report
 from . import municipal_retention_patent_report
 from . import municipal_retention_xlsx_report
