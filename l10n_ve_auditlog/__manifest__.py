5c351cfb0818b5871a574a80d394f355f6f37bee
andyengit <anderson.armeya@gmail.com>
2026-08-10 03:35:42 +0000

    [UPD] l10n_ve_auditlog: increment version and enhance XML-ID management
    - Incremented version to 18.0.1.2.2 in the module manifest.
    - Introduced a new function to ensure proper handling of XML-IDs for database audit tables, improving migration reliability.
    - Refactored pre-migrate scripts to streamline XML-ID reassignment and prevent duplicates during migration.
    - Enhanced the create method in the db_audit_table model to support multi-record creation while maintaining existing records.

--- l10n_ve_auditlog/__manifest__.py
+++ l10n_ve_auditlog/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Venezuela - Audit Log",
     "summary": "ORM and SENIAT fiscal audit log for Venezuela",
-    "version": "18.0.1.2.1",
+    "version": "18.0.1.2.2",
     "author": "ABF OSIELL, Anderson Armeya, andyengit, Odoo Community Association (OCA)",
     "maintainers": ["andyengit"],
     "license": "AGPL-3",
